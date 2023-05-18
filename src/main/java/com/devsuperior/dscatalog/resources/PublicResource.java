8dee9cb8d3db7745bf589a30f9f1b562ebb4c927
josemateus <josemateus.ufma@gmail.com>
2023-05-17 22:49:50 -0300

    public resources

--- src/main/java/com/devsuperior/dscatalog/resources/PublicResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/PublicResource.java
@@ -1,6 +1,14 @@
 package com.devsuperior.dscatalog.resources;
 
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +20,12 @@ import java.time.LocalDate;
 @RequestMapping("public")
 public class PublicResource {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
-    public String getPublic(HttpServletRequest request) {
-        return String.format("Data: %s Remote IP: %s Header IP %s", LocalDate.now().toString(), request.getRemoteAddr(), request.getHeader("X-FORWARDED-FOR"));
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
     }
 }
